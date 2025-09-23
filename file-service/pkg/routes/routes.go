41234273af2d486612b82cbaf8d492f24cde3bb6
Dhairya Majmudar <2022kuec2045@iiitkota.ac.in>
2025-09-23 22:23:17 +0530

    feat (file-service): service routes

---
+++ file-service/pkg/routes/routes.go
@@ -0,0 +1,25 @@
+package routes
+
+import (
+	"volt/file-service/pkg/controllers"
+	"volt/file-service/pkg/middlewares"
+
+	"github.com/gorilla/mux"
+)
+
+func SetupRoutes(router *mux.Router) {
+	api := router.PathPrefix("/api/v1").Subrouter()
+
+	// Health check endpoint
+	api.HandleFunc("/health", controllers.HealthCheck).Methods("GET")
+
+	protected := api.PathPrefix("").Subrouter()
+	protected.Use(middlewares.AuthMiddleware)
+
+	// File upload and management routes
+	protected.HandleFunc("/files/upload", controllers.UploadFile).Methods("POST")
+	protected.HandleFunc("/files", controllers.GetFiles).Methods("GET")
+	protected.HandleFunc("/files/{ID}", controllers.GetFileByID).Methods("GET")
+	protected.HandleFunc("/files/{ID}", controllers.DeleteFile).Methods("DELETE")
+	protected.HandleFunc("/users/storage-stats", controllers.GetUserStorageStats).Methods("GET")
+}
