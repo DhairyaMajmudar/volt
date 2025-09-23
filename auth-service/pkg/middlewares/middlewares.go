a416d75f30c4a40832057e52da5ed07402edb933
Dhairya Majmudar <2022kuec2045@iiitkota.ac.in>
2025-09-23 21:44:18 +0530

    feat (auth-service): add middlewares for cors and logging

---
+++ auth-service/pkg/middlewares/middlewares.go
@@ -0,0 +1,36 @@
+package middlewares
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"strings"
+
+	"volt/auth-service/pkg/utils"
+)
+
+type contextKey string
+
+const UserContextKey contextKey = "user"
+
+func CorsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s", r.Method, r.RequestURI, r.RemoteAddr)
+		next.ServeHTTP(w, r)
+	})
+}
