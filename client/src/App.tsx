656cc89852f18537939de2a19bb867203db127a0
Dhairya Majmudar <2022kuec2045@iiitkota.ac.in>
2025-09-25 12:40:38 +0530

    feat (client): app page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,16 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import RegisterPage from './pages/RegisterPage';
+
 export function App() {
-  return <div>Hello</div>;
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
